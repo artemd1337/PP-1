ff3fbd80eecc752155f48eb5374bff4e8d0fd6dc
artiom.danilov@outlook.com <artiom.danilov@outlook.com>
2022-11-19 08:14:18 +0400

    Написана еще 1 реализация алгоритма, чтобы сравнить производительность.

--- Source.cpp
+++ Source.cpp
@@ -12,6 +12,9 @@
 #include <windows.h>
 using namespace std;
 
+bool EXIT = false;
+std::string ANSWER;
+
 bool increment_str(string &str, const string& alphabet) {
 	for (size_t i = 0; i < str.length(); ++i) {
 		size_t numInAplhabet = alphabet.find(str[str.length() - 1 - i]);
@@ -28,7 +31,55 @@ bool increment_str(string &str, const string& alphabet) {
 	}
 }
 
+bool bruteOneThread(const size_t passlength, const string& hash, int block_size, const string& alphabet) {
+	std::mutex mu;
+	digestpp::sha3 hasher(256);
+	string text(passlength, alphabet[0]);
+	text[0] = alphabet[block_size * omp_get_thread_num()];
+	int k = 0;
+	int max_k = 2 * block_size;
+	std::string hex;
+	for (int i = 0; i < passlength - 1; ++i) {
+		max_k *= alphabet.length();
+	}
+	double t1 = omp_get_wtime();
+	for (k = 0; k < max_k && !EXIT; ++k) {
+		hasher.absorb(text);
+		hex = hasher.hexdigest();
+		if (hex == hash) {
+			double t2 = omp_get_wtime();
+			ANSWER = text;
+			EXIT = true;
+			return true;
+		}
+		hasher.reset();
+		auto ret = increment_str(text, alphabet);
+		if (ret == false) {
+			return false;
+		}
+	}
+	return false;
+}
 
+pair<string, double> bruteforce_edited(const size_t passlength, const string& hash, const string& alphabet) {
+	digestpp::sha3 hasher(256);
+	string text(passlength, alphabet[0]);
+	int k = 0;
+	size_t i = 0;
+	int completed = 0;
+	std::string hex;
+	std::string answer;
+	double t1 = omp_get_wtime();
+	std::mutex mu;
+	omp_set_num_threads(12);
+	#pragma omp parallel shared(hash, passlength, alphabet)
+	{
+		int block = alphabet.length() / omp_get_num_threads();
+		bruteOneThread(passlength, hash, block, alphabet);
+	}
+	double t2 = omp_get_wtime();
+	return { answer , t2-t1};
+}
 
 pair<string, double> bruteforce(const size_t passlength, const string& hash, const string& alphabet) {
 	static bool exit = false;
@@ -40,32 +91,30 @@ pair<string, double> bruteforce(const size_t passlength, const string& hash, con
 	std::string hex;
 	std::string answer;
 	double t1 = omp_get_wtime();
-	std::mutex mu;
-	omp_set_num_threads(1);
+	omp_set_num_threads(12);
 	#pragma omp parallel for shared(text, hash, hasher, passlength, alphabet, exit, answer) private(k, i, hex)
 	for (k = 0; k < 1000000; ++k) {
 		hasher.absorb(text);
-		mu.lock();
-		hex = hasher.hexdigest();
-		if (hex == hash) {
-			answer = text;
-			exit = true;
-			mu.unlock();
-			break;
-		}
-		mu.unlock();
-		hasher.reset();
-		if (!exit) {
-			auto res = increment_str(text, alphabet);
-			if (res == false) {
+		#pragma omp critical
+		{
+			hex = hasher.hexdigest();
+			if (hex == hash) {
+				answer = text;
 				exit = true;
-				break;
 			}
 		}
-		else break;
+		if (exit) {
+			break;
+		}
+		hasher.reset();
+		auto res = increment_str(text, alphabet);
+		if (res == false) {
+			exit = true;
+			break;
+		}
 	}
 	double t2 = omp_get_wtime();
-	return { answer , t2-t1};
+	return { answer , t2 - t1 };
 }
 
 vector<string> readHash(const string& filepath) {
@@ -111,7 +160,9 @@ int main() {
 	srand(time(0));
 	auto hashVector = readHash("hash.txt");
 	for (auto hash : hashVector) {
-		auto result = bruteforce(3, hash, "abcdefghijklmnopqrstuvwxyz0987654321");
+		auto result = bruteforce_edited(4, hash, "abcdefghijklmnopqrstuvwxyz0987654321");
+		std::cout << ANSWER << ' ' << result.second << '\n';
+		result = bruteforce(4, hash, "abcdefghijklmnopqrstuvwxyz0987654321");
 		std::cout << result.first << ' ' << result.second << '\n';
 		writeHashWithResult("result.txt", hash, result.first);
 	}
@@ -127,107 +178,3 @@ int main() {
 	std::cout << "AVERAGE DURATION WITH LENGTH 1: " << averageTime / 20 << std::endl;
 	*/
 }
-
-/*
-pair<string, double> bruteforce(size_t passlength, string hash, string alphabet){
-	static bool exit = false;
-	digestpp::sha3 hasher(256);
-	string text(passlength, alphabet[0]);
-	int completed = 0;
-	int k = 0;
-	size_t i = 0;
-	auto t1 = omp_get_wtime();
-	omp_set_num_threads();
-	#pragma omp parallel for shared(passlength, hash, completed, alphabet, text) private(k, i)
-	for (k = 0; k < 100000000; ++k) {
-		hasher.absorb(text);
-		auto hex = hasher.hexdigest(); // Получить хэш в виде hex-строки
-		if (hex == hash) {
-			cout << text << endl;
-			completed = 1;
-			break;
-		}
-		hasher.reset();
-		for (i = 0; i < passlength && completed != -1; ++i) {
-			size_t numInAplhabet = alphabet.find(text[text.length() - 1 - i]);
-			if (numInAplhabet == alphabet.length() - 1) {
-				text[text.length() - 1 - i] = alphabet[0];
-				if (i == passlength - 1) {
-					completed = -1;
-					break;
-				}
-			}
-			else {
-				text[text.length() - 1 - i] = alphabet[numInAplhabet + 1];
-				break;
-			}
-		}
-		if (completed == -1) {
-			break;
-		}
-	}
-	auto t2 = omp_get_wtime();
-	if (completed == 1)
-		return { text, t2-t1 };
-	else
-		return { string(), -1 };
-
-}
-
-vector<string> readHash(const string& filepath) {
-	ifstream input(filepath);
-	string hash;
-	vector<string> ans;
-	while (input >> hash) {
-		if (!hash.empty()) ans.push_back(hash);
-	}
-	return ans;
-}
-
-void writeHashWithResult(const string& filepath, const string& hash, const string& result) {
-	ofstream output(filepath, std::ios_base::app);
-	output << hash << ':' << result << std::endl;
-}
-
-string getRandomString(size_t length) {
-	string result;
-	for (size_t i = 0; i < length; ++i) {
-		int randnum = rand() % 36;
-		if (randnum < 10)
-			result.push_back(char('0' + randnum));
-		else
-			result.push_back(char('a' + randnum - 10));
-	}
-	return result;
-}
-
-string getHash(string str) {
-	digestpp::sha3 hasher(256);
-	hasher.absorb(str);
-	auto hex = hasher.hexdigest();
-	return hex;
-}
-
-void logCalculation(string& str, string& hash, pair<string, double> bruteforceResult) {
-	ofstream output("BruteforceLog.txt", std::ios_base::app);
-	output << str << " : " << hash << " : " << bruteforceResult.second << " ms : " << bruteforceResult.first << std::endl;
-}
-
-int main() {
-	srand(time(0));
-	auto hashVector = readHash("hash.txt");
-	for (auto hash : hashVector) {
-		auto result = bruteforce(3, hash, "abcdefghijklmnopqrstuvwxyz0987654321");
-		writeHashWithResult("result.txt", hash, result.first);
-	}
-
-	long double averageTime = 0;
-	for (size_t i = 0; i < 5; ++i) {
-		string str = getRandomString(4);
-		auto hash = getHash(str);
-		auto result = bruteforce(4, hash, "abcdefghijklmnopqrstuvwxyz0987654321");
-		logCalculation(str, hash, result);
-		//averageTime += result.second;
-	}
-	//std::cout << "AVERAGE DURATION WITH LENGTH 1: " << averageTime / 5 << std::endl;
-}*/
